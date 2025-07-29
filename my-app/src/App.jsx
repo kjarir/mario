d99d0b0456909e767ac475c9b49f429f35d0ab93
kjarir <kjarir23@gmail.com>
2025-07-29 16:49:55 +0530

    added lenis

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,12 @@
 import Index from './pages/index'
 import NavbarComponent from './components/Navbar'
+import { useLenis } from './hooks/useLenis'
 import './output.css'
 
 function App() {
+  // Initialize Lenis smooth scrolling
+  useLenis();
+
   return (
     <div className="min-h-screen">
       <NavbarComponent />
